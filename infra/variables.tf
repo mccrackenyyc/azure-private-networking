aab52e081fb40fd538db34daddb84a4c6bbaccec
mccrackenyyc <smccracken@live.ca>
2023-04-24 20:56:04 -0600

    Environment building

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,17 @@
 variable "subscription_id" {
-    description = "Azure subscription"
-    type = string
-    default = "00000000-0000-0000-0000-000000000000"
+  description = "Azure subscription"
+  type        = string
+  default     = "20c17ce1-c880-4374-ab18-0c3a72158cf7"
+}
+
+variable "env_name" {
+  description = "Working environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "exampletag" {
+  description = "Example use of a tag"
+  type        = string
+  default     = "exampletag"
 }
